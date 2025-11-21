b859bea0b6661358c8f3059f5508db9fd84e5ffe
Vineet Pancheshwar <vineetpancheshwar1611@gmail.com>
2025-11-21 13:48:45 +0530

    fix: Update CORS origin to include the API endpoint

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://rsat.ricr.in/'
+        'https://rsat.ricr.in/api'
     ],
     credentials: true
 }));
