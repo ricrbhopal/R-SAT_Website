d3c0a527022905bd151c732609c5262b9ed48df0
Vineet Pancheshwar <vineetpancheshwar1611@gmail.com>
2025-12-01 12:36:03 +0530

    feat: Add Caller model with schema for student, admin, and related references

---
+++ server/src/models/callerModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const callerSchema = new mongoose.Schema({
+  student_ID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Student",
+  },
+  admin_ID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Admin",
+  },
+  reffered_ID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Referred",
+  },
+  admintCard_ID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "AdmitCard",
+  },
+  demoClass_ID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Demo",
+  },
+  result: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Result",
+  },
+});
+
+const Caller = mongoose.model("Caller", callerSchema);
+
+export default Caller;
