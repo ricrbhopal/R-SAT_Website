196579ce2aa026dc4020f17cd2b1566ef8bf45b3
Vineet Pancheshwar <vineetpancheshwar1611@gmail.com>
2025-12-09 16:48:08 +0530

    fix: Correct CORS allowed origin for production environment

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://rsat.ricr.in"
+  "https://rsat.ricr.in/api",
 ];
 
 app.use(cors({ 
